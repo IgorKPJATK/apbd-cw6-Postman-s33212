c77563348d94020ad2f57d4fb561d6babd4ed599
Igor Kozuchowski <s33212@pjwstk.edu.pl>
2026-04-21 23:33:47 +0200

    Dodanie klasy rezerwacji

---
+++ zad5/Models/Reservation.cs
@@ -0,0 +1,34 @@
+﻿namespace zad5.Models;
+
+using System.ComponentModel.DataAnnotations;
+
+public class Reservation : IValidatableObject
+{
+        public int Id { get; set; }
+        public int RoomId { get; set; }
+        
+        [Required]
+        public string OrganizerName { get; set; }
+        
+        [Required]
+        public string Topic { get; set; }
+        
+        [Required]
+        public DateOnly Date { get; set; }
+        
+        [Required]
+        public TimeSpan StartTime { get; set; }
+        
+        [Required]
+        public TimeSpan EndTime { get; set; }
+        
+        public string Status { get; set; } 
+        
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (EndTime <= StartTime)
+            {
+                yield return new ValidationResult("Godzina zakończenia musi być późniejsza niż rozpoczęcia.", new[] { nameof(EndTime) });
+            }
+        }
+}
