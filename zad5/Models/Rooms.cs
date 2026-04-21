5aaaa27f25c3bcf3e50a8faf219ab293c1228a6a
Igor Kozuchowski <s33212@pjwstk.edu.pl>
2026-04-21 23:30:52 +0200

    dodanie klasy z Pokojami

---
+++ zad5/Models/Rooms.cs
@@ -0,0 +1,22 @@
+﻿namespace zad5.Models;
+
+using System.ComponentModel.DataAnnotations;
+
+public class Rooms
+{
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "Nazwa sali jest wymagana.")]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage = "Kod budynku jest wymagany.")]
+        public string BuildingCode { get; set; }
+        
+        public int Floor { get; set; }
+        
+        [Range(1, 500, ErrorMessage = "Pojemność musi być między 1 a 500.")]
+        public int Capacity { get; set; }
+        
+        public bool HasProjector { get; set; }
+        public bool IsActive { get; set; }
+}
